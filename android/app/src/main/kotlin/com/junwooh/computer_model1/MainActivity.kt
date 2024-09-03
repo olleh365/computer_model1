963f486dd65b4b4d6303c180de13d41034e5f81a
olleh365 <olleh365@gmail.com>
2024-09-03 20:56:19 +0900

    init commit

---
+++ android/app/src/main/kotlin/com/junwooh/computer_model1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.junwooh.computer_model1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              
+            }, child: const Text('버튼'),
+          ),
+        ),
+      ),
+    );
+  }
+}
